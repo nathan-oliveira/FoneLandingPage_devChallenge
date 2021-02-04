fd128613e4dbead19b320ccbc8fb3172619d6a99
Nathan Oliveira <nathan_oliveiramendonca@hotmail.com>
2021-02-04 17:03:49 -0300

    remove maps

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,6 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sass`,
     `gatsby-plugin-styled-components`,
-    `gatsby-plugin-sitemap`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
